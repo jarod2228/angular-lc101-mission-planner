1614a1dc294fbbdf2e64dfcc9963b8a1476c3c52
Jarod Waggener <jarod2228@rocketmail.com>
2021-04-02 16:20:22 -0500

    attempting to rework equipment for bonus mission, stuck on the save function. Cannot make it work with a string isntead of an object. It keeps replacing the very last item in the array instead of the 'piece' of equipment and I canNOT figure out why my reference of 'piece' in the save function in equipment.component.ts is coming up as -1 and it doesn't find that item in the array. it seems like i've tried everything.

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: string[] = [
+    "Habitat",
+    "Drones",
+    "Food containers",
+    "Oxygen tanks"
+  ];
+
+  pieceBeingEdited: string = null;
+
   constructor() { }
 
   ngOnInit() {
   }
-
+add(pieceName: string) {
+  this.equipment.push(pieceName);
+}
+remove(piece: string) {
+  let index = this.equipment.indexOf(piece);
+  this.equipment.splice(index,1);
+}
+edit(piece: string) {
+  this.pieceBeingEdited = piece;
+}
+save(piece: string) {
+  let index = this.equipment.indexOf(piece);
+  this.equipment.splice(index, 1, piece);
+  this.pieceBeingEdited = null;
+}
 }
